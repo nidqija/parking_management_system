22e9392d7c433a25f12e1dbac1b3160b1e2616a4
C2Meng <chancm530@gmail.com>
2026-02-11 04:59:43 +0800

    report panel for vehicle list now available

--- View/ReportPanel.java
+++ View/ReportPanel.java
@@ -1,19 +1,50 @@
 package View;
-
+import java.util.List;
+import java.util.ArrayList;
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+
+import Model.Vehicle;
 
 public class ReportPanel extends JPanel {
 
       private CardLayout cardLayout;
       private JPanel contentPanel;
+      private JTable vehicleTable;
+      private DefaultTableModel tableModel;
+
+       private void loadVehicleData() {
+       tableModel.setRowCount(0); // clear existing rows
+
+        List<Vehicle> vehicles = Vehicle.getVehicleReportList();
+        int i = 0;
+        for (Vehicle v : vehicles) {
+            tableModel.addRow(new Object[]{
+                    i + 1,
+                    v.getPlateNumber(),
+                    v.getType(),
+                    v.getTicketNumber() != null ? v.getTicketNumber() : "",
+                    v.getSpotID() != null ? v.getSpotID() : "",
+                    v.getEntryTime() != null ? v.getEntryTime().toString() : "",
+                    v.getExitTime() != null ? v.getExitTime().toString() : "",
+                    v.getPaymentStatus() != null ? v.getPaymentStatus() : ""
+                    //ternary operator
+                    //condition ? value_if_true : value_if_false
+            });
+            i++; //increment
+        }
+    }
     
 
 
       public ReportPanel(MainFrame mainFrame) {
+        setLayout(new BorderLayout()); // main panel uses BorderLayout
 
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // padding
 
         JLabel label = new JLabel("Report Panel - Parking Management System");
         label.setFont(new Font("Arial", Font.BOLD, 24));
@@ -51,7 +82,7 @@ public class ReportPanel extends JPanel {
 
         panel.add(label);
         panel.add(buttonPanel);
-        add(panel);
+        add(panel, BorderLayout.NORTH);
 
        //i want the pages to like switch based on what button is pressed to show each report basically
           
@@ -61,11 +92,37 @@ public class ReportPanel extends JPanel {
       JPanel vehiclePanel = new JPanel(new BorderLayout());
       JLabel vehicleLabel = new JLabel("List of Vehicles", SwingConstants.CENTER);
       vehicleLabel.setFont(new Font("Arial", Font.BOLD, 20));
-      vehiclePanel.add(vehicleLabel, BorderLayout.CENTER);
-
-
+      vehiclePanel.add(vehicleLabel, BorderLayout.NORTH);
 
       
+
+      String[] columnNames = { "Vehicle" ,"Lisence Plate", "Vehicle Type", "Ticket ID", "Parking Spot", "Entry Time", "Exit Time", "Payment Status"};
+
+
+       // Vehicle JTable panel
+
+      DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+      centerRenderer.setHorizontalAlignment(SwingConstants.CENTER);
+
+      tableModel = new DefaultTableModel(columnNames, 0) {
+          @Override
+          public boolean isCellEditable(int row, int column) {
+              return false; // make table non-editable
+          }
+        };
+        // JTable
+        vehicleTable = new JTable(tableModel);
+        vehicleTable.setFillsViewportHeight(true);
+        vehicleTable.setRowHeight(25);
+        vehicleTable.setFont(new Font("Arial", Font.PLAIN, 14));
+        vehicleTable.getTableHeader().setFont(new Font("Arial", Font.BOLD, 16));
+        vehicleTable.getColumnModel().getColumn(0).setCellRenderer(centerRenderer); //centers vehicle column
+        // Scroll pane
+        JScrollPane scrollPane = new JScrollPane(vehicleTable);
+        vehiclePanel.add(scrollPane, BorderLayout.CENTER); 
+
+        loadVehicleData();
+    
 
 
       JPanel revenuePanel = new JPanel(new BorderLayout());
@@ -73,6 +130,7 @@ public class ReportPanel extends JPanel {
       revenueLabel.setFont(new Font("Arial", Font.BOLD, 20));
       revenuePanel.add(revenueLabel, BorderLayout.CENTER);
       
+   
 
       JPanel occupancyPanel = new JPanel(new BorderLayout());
       JLabel occupancyLabel = new JLabel("Occupancy Report", SwingConstants.CENTER);
